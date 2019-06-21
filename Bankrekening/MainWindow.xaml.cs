7640f7469bc0349a539b911864d48ef0834f7a25
LUCA classens <lclassens@hotmail.com>
2019-06-21 18:01:25 +0200

    Ja

---
+++ Bankrekening/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using Bankrekening.classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bankrekening
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //public baseRekening newRekening;
+        public enum rekeningen { Luca, Henk};
+        public baseRekening left;
+        public baseRekening right;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            left = new baseRekening(rekeningen.Luca.ToString());
+            right = new baseRekening(rekeningen.Henk.ToString());
+
+            left.WriteFile();
+
+            lblNameLeft.Content = left.Naam;
+            lblNumLeft.Content = left.Rekeningnummer;
+
+            lblNameRight.Content = right.Naam;
+            lblNumRight.Content = right.Rekeningnummer;
+        }
+
+        // Left
+        private void BtnTakeLeft_Click(object sender, RoutedEventArgs e)
+        {
+            int request = Convert.ToInt32(tbxLeft.Text);
+
+            int taken = left.Take(request);
+            lblMonLeft.Content = taken.ToString();
+        }
+
+        private void BtnStortLeft_Click(object sender, RoutedEventArgs e)
+        {
+            int request = Convert.ToInt32(tbxLeft.Text);
+
+            int add = left.Add(request);
+            lblMonLeft.Content = add.ToString();
+        }
+
+        // Right
+        private void BtnTakeRight_Click(object sender, RoutedEventArgs e)
+        {
+            int request = Convert.ToInt32(tbxRight.Text);
+
+            int taken = right.Take(request);
+            lblMonRight.Content = taken.ToString();
+
+        }
+
+        private void BtnStortRight_Click(object sender, RoutedEventArgs e)
+        {
+            int request = Convert.ToInt32(tbxRight.Text);
+
+            int add = right.Add(request);
+            lblMonRight.Content = add.ToString();
+        }
+
+
+    }
+}
---
+++ Bankrekening/classes/baseRekening.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Bankrekening.classes
+{
+    public class baseRekening
+    {
+        private int rekeningnummer;
+        private string naam;
+        private int saldo;
+        private static int volgendNummer = 1;
+
+        public baseRekening left;
+        public int Rekeningnummer { get; set; }
+        public string Naam { get; set; }
+        public int Saldo { get { return saldo; } set { saldo = value; } }
+
+        public baseRekening(string naam)
+        {
+            Naam = naam;
+            Saldo = 0;
+            Rekeningnummer = volgendNummer;
+            volgendNummer++;
+        }
+
+        public baseRekening()
+        {
+            //Hier volgt normaal code, maar dit is een voorbeeld
+        }
+
+        public override string ToString()
+        {
+            return string.Concat(Saldo, " ", Naam);
+        }
+
+        public void WriteFile()
+        {
+            string pad = @"E:\verplaats\map1\" + "Bank" + ".txt";
+
+            try
+            {
+                if (File.Exists(pad))
+                {
+                    //Console.WriteLine("bestaat al met de naam: " + pad);
+                    //MessageBox.Show("bestaat al met de naam: " + pad);
+                    using (StreamReader reader = File.OpenText(pad))
+                    {
+                        string text = "";
+                        if((text = reader.ReadLine()) == null)
+                        {
+                            ToString();
+                        }
+                        while ((text = reader.ReadLine()) != null)
+                        {
+                            //Console.WriteLine("Dit is de text in het bestand: " + text);
+                            MessageBox.Show("Dit is de text in het bestand: " + text);
+                        }
+                        
+                    }
+                    //reader.Close();
+                }
+
+                else
+                {
+                    File.Create(pad);
+                    //Console.WriteLine("file gemaakt met de naam " + pad);
+                    MessageBox.Show("file gemaakt met de naam" + pad);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                //reader.Close();
+            }
+        }
+
+        public int NeemOp(int bedrag)
+        {
+            Saldo = Saldo - bedrag;
+
+            return Saldo;
+        }
+
+        public int currentSaldo()
+        {
+            return Saldo;
+        }
+
+        public int Add(int request)
+        {
+            Saldo = Saldo + request;
+
+            return Saldo;
+        }
+
+        public int Take(int request)
+        {
+            if (Saldo - request < 0)
+            {
+                MessageBox.Show("You can't go below 0");
+                return this.Saldo;
+            }
+            else
+            {
+                return NeemOp(request);
+            }
+
+        }
+
+    }
+}
